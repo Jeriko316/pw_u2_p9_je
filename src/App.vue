ab3aeeb04d163a6ee6a3685644ab5983d7ea3f70
jeriko316 <jkenriquez@hotmail.com>
2025-11-21 19:39:53 -0500

    taller 13 a

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <template>
-  
+  <div>
+    <ContadorView/>
+  </div>
 </template>
 
 <script>
+import ContadorView from "../src/views/ContadorView.vue";
 export default {
-
-}
+  components: {
+    ContadorView,
+  },
+};
 </script>
 
 <style>
-
 </style>
---
+++ src/views/ContadorView.vue
@@ -0,0 +1,16 @@
+<template>
+   <Contador/>
+</template>
+
+<script>
+import Contador from "../components/Contador.vue"
+export default {
+    components: {
+        Contador
+    }
+}
+</script>
+
+<style>
+
+</style>
